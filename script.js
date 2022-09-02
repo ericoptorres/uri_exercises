1b9705ef282fceb5ae88cb312310302025cea2a2
erico <erico_pt@hotmail.com>
2022-09-02 16:14:16 -0300

    teste

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const { Console } = require('console');
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('/');
 
+
+
+
+alterado
+
 var dia = lines.shift()
 var mes = lines.shift()
 var ano = lines.shift()
