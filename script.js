44b17edfd2967bd1417dec03c3286805c07f9b26
erico <erico_pt@hotmail.com>
2022-09-11 18:31:35 -0300

    Added passou function

--- script.js
+++ script.js
@@ -4,52 +4,6 @@ var lines = input.split('\n');
 var prompt = require('prompt-sync')();
 
 
-//12 iterações
-//x + y > 12 e (x == y)false
-
-let m = 12
-
-let n = 12
-
-let aux = []
-let mat = []
-let valor
-let soma = 0
-let operador = lines.shift().trim()
-let z = -1
-
-while (m--){
-  n = 12
-
-  aux = []
-  while (n--){
-    valor = parseFloat(lines.shift())
-    aux.push(valor)
-  }
-  mat.push(aux)
-}
-
-m = 12
-
-n = 12
-
-
-while (m--){
-  n = 12
-  while(n--){
-    valor = mat[m][n]
-    if((m > n) && (m + n) > 11){
-      soma = soma + valor
-      z++
-    }
-  }
-}
-
-if (operador == "S"){
-  console.log(soma.toFixed(1))
-}
-else if (operador == "M"){
-  console.log(soma / z )
-}
+//O aluno consiste em um objeto com as seguintes propriedades: id, nome, sala, media e presença.
 
 
