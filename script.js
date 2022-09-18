84cf39ba757a0e8b4f8fef28804bdfb532518a6a
erico <erico_pt@hotmail.com>
2022-09-17 22:41:40 -0300

    exercising matrix

--- script.js
+++ script.js
@@ -5,6 +5,33 @@ var lines = input.split('\n');
 
 var prompt = require('prompt-sync')();
 
+let m = 0
+let n = 0
+let number = 4
+let array = []
+let matriz = []
 
-//O aluno consiste em um objeto com as seguintes propriedades: id, nome, sala, media e presença.
+while (number){
+
+    while (m < number){
+        while (n < number){
+            if (m == 3){
+                array[n] = 1
+                
+            }
+            else{
+                array[n] = 2
+                
+            }
+            n++
+        }
+        n = 0
+        
+        matriz.push(array)
+        console.log(matriz)
+        m++
+    }
+    console.log(matriz)
+    number = 0
+}
 
