331839f531ccbb5a35eb26f387c4fdfbd76ff417
erico <erico_pt@hotmail.com>
2022-09-18 15:29:02 -0300

    uri 1435

--- script.js
+++ script.js
@@ -5,33 +5,4 @@ var lines = input.split('\n');
 
 var prompt = require('prompt-sync')();
 
-let m = 0
-let n = 0
-let number = 4
-let array = []
-let matriz = []
-
-while (number){
-
-    while (m < number){
-        while (n < number){
-            if (m == 3){
-                array[n] = 1
-                
-            }
-            else{
-                array[n] = 2
-                
-            }
-            n++
-        }
-        n = 0
-        
-        matriz.push(array)
-        console.log(matriz)
-        m++
-    }
-    console.log(matriz)
-    number = 0
-}
 
