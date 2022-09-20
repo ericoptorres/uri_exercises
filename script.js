b74f884a921c9d140a056b410196d84c2df15417
erico <erico_pt@hotmail.com>
2022-09-19 21:31:10 -0300

    1957

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const { clearScreenDown } = require('readline');
-
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
 var prompt = require('prompt-sync')();
-
 
