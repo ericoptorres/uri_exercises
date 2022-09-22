dbb6430428c43d81165d2ae99a2c70990e4eeb1f
erico <erico_pt@hotmail.com>
2022-09-22 20:56:32 -0300

    2756

--- script.js
+++ script.js
@@ -3,11 +3,3 @@ var lines = input.split('\n');
 
 var prompt = require('prompt-sync')();
 
-let number = parseInt(lines.shift())
-
-let i = 1
-
-while (i <= number){
-    console.log(i, i*i, i*i*i)
-    i++
-}
