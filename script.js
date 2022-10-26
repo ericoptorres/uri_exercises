598a6758e278ce64d7408cbe3f48f6c9e719240d
erico <erico_pt@hotmail.com>
2022-10-26 13:21:31 -0300

    Freecode reverse string

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
+function reverseString(str) {
+    let newStr
+    let aux = []
+    let aux2
+    let finalStr
 
+    newStr = str.split('')
+
+    let i = newStr.length
+    while (i--){
+        aux2 = newStr.pop()
+        aux.push(aux2)
+    }
+    
+    finalStr = aux.join('')
+    return finalStr;
+  }
+  
+  reverseString("htraE morf sgniteerG");
 
 
