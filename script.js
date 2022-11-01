dd1e838d131c8813a7a2a564a686d34baa67449a
erico <erico_pt@hotmail.com>
2022-11-01 20:41:12 -0300

    1038 - Snack

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
-function mutation(arr) {
-
-    let i = 0
-    while (i < arr[1].length){
-        
-    }
-
-
-    arr[0].indexof(arr[1])
-
-
-
-
-  return arr;
-}
-
-mutation(["hello", "hey"]);
