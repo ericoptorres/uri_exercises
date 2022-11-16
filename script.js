9a8f68050bf5d7c2a3ad2ab5e053c5aa66b66511
erico <erico_pt@hotmail.com>
2022-11-16 11:41:11 -0300

    Freecode - Sum All

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
 
+function diffArray(arr1, arr2) {
+  const newArr = [];
+
+  if (arr1.some()  ){
+    
+  }
+
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
