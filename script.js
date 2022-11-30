018f95b67b26290f41414360916264e98d5c8081
erico <erico_pt@hotmail.com>
2022-11-30 18:56:36 -0300

    Freecode - Pig Latin

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
+function steamrollArray(arr) {
+
+    const endless = []
+
+    const flatening = (array => {
+        array.map(element => {
+            if (Array.isArray(element)){
+                return flatening(element)
+                
+            }
+            console.log(`retorno final: ${element}`)
+            endless.push(element)
+            return element
+            
+
+        })
+    })
+
+    console.log(flatening(arr))
+
+    console.log(endless)
+    return endless;
+  }
+  
+  steamrollArray([1, {}, [3, [[4]]]]);
 
