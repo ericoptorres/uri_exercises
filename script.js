a32de65d94d95dc021e9ed9624c3efeeec4b503b
erico <erico_pt@hotmail.com>
2022-12-01 14:49:51 -0300

    Freecode - Binary Agents

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
-function steamrollArray(arr) {
-
-    const endless = []
-
-    const flatening = (array => {
-        array.map(element => {
-            if (Array.isArray(element)){
-                return flatening(element)
-                
-            }
-            console.log(`retorno final: ${element}`)
-            endless.push(element)
-            return element
-            
-
-        })
-    })
-
-    console.log(flatening(arr))
-
-    console.log(endless)
-    return endless;
-  }
-  
-  steamrollArray([1, {}, [3, [[4]]]]);
-
